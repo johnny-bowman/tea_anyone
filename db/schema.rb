8aea5ee92ca1389cf0d3bb58f543327555624c24
Johnny Bowman <johntbowman.production@gmail.com>
2022-08-04 11:59:21 -0600

    DB: Update subscription and tea table columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_034346) do
+ActiveRecord::Schema.define(version: 2022_08_04_175721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "subscriptions", force: :cascade do |t|
-    t.string "title"
-    t.float "price"
+    t.float "price", default: 0.0
     t.integer "status"
     t.integer "frequency"
     t.bigint "user_id"
@@ -40,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_08_03_034346) do
     t.integer "brew_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
   end
 
   create_table "users", force: :cascade do |t|
