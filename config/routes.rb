d54f912a3f3322d1bf9eb3085dc7c8ae71305ec7
Johnny Bowman <johntbowman.production@gmail.com>
2022-08-05 10:35:42 -0600

    update seed file for cloning

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :subscription
+      resources :subscription, only: [:create, :update]
       get 'users/:id/subscriptions', to: "user_subscription#index"
     end
   end
