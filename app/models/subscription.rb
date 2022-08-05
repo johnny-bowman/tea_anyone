f9c78edc93fc7c97dee9245e550ce7df791ac008
Johnny Bowman <johntbowman.production@gmail.com>
2022-08-04 23:41:16 -0600

    Model: subscription model validates numericailty of price and frequency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,5 +3,9 @@ class Subscription < ApplicationRecord
   has_many :tea_subscriptions
   has_many :teas, through: :tea_subscriptions
 
-  validates_presence_of :title, :price, :status, :frequency
+  validates_presence_of :user_id, :price, :status, :frequency
+  validates :price, numericality: true
+  validates :frequency, numericality: true
+
+  enum status: { active: 0, cancelled: 1}
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe Subscription, type: :model do
   end
 
   describe "validations" do
-    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price) }
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:frequency) }
+    it { should validate_numericality_of(:frequency) }
   end
 
   describe "class methods" do
